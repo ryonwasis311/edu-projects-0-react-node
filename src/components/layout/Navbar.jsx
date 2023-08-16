6890be8228a1a150fbf0bc2963fdd4407517d3d3
ryonwasis <Mat.pavel08@gmail.com>
2023-08-16 07:43:03 -0700

    Navbar_is_going

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code" /> DevConnector
+        </Link>
+      </h1>
+      <Fragment>{guestLinks}</Fragment>
+    </nav>
+  );
+};
+
+export default Navbar;
